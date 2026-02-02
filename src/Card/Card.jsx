628a04c6588b70925fe711d3ed35dd4e136dc6e5
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-02-02 23:52:31 +0530

    new update

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -4,7 +4,7 @@ function Card({ location, country, name }) {
   return (
     <div className='bg-slate-800 rounded-md font-bold w-32 h-44 flex justify-center '>
       <h1 className='text-fuchsia-50 flex justify-center items-center'>Hii this side {name} from {country}</h1>
-
+    <h2>Footer</h2>
     </div>
   )
 }
