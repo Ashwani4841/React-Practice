688d3e4e05df07d962a6789b89a749cf6f0dcc0a
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-03-17 23:33:26 +0530

    Add useState React practice project

--- useState/src/components/User.jsx
+++ useState/src/components/User.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function User() {
+function User(props) {
   return (
     <div>
-      <h3>Price :</h3>
-      <h3>Models :</h3>
-      <h3>Models :</h3>
+      <h3>Price :{props.Price}</h3>
+      <h3>Models :{props.Model}</h3>
+      <h3>Location :{props.Location}</h3>
     </div>
   )
 }
--- useState/src/main.jsx
+++ useState/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import User from './components/User.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <User Price="9999" Model="Vivo" Location="Noida"/>
   </StrictMode>,
 )
