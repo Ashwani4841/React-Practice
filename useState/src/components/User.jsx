04e4f368f762a4f69f2e1ff3a9de12c4ce6374c3
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-03-17 23:11:07 +0530

    Add useState React practice project

---
+++ useState/src/components/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>
+      <h3>Price :</h3>
+      <h3>Models :</h3>
+      <h3>Models :</h3>
+    </div>
+  )
+}
+
+export default User
